23de52beff103d22e302bad7eff8691e7ff0baa1
HM2 Joseph Nolasco <jjcnolasco777@gmail.com>
2025-11-07 17:55:13 -0800

    Completed Dynamic Material Instances tutorial

---
+++ Source/firstperson415/CubeDMIMod.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Components/BoxComponent.h"
+#include "CubeDMIMod.generated.h"
+
+UCLASS()
+class FIRSTPERSON415_API ACubeDMIMod : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACubeDMIMod();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Box Component
+	UPROPERTY(EditAnywhere)
+		UBoxComponent* boxComp;
+
+	// Cube Mesh
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* cubeMesh;
+
+	// Base Material
+	UPROPERTY(EditAnywhere)
+		UMaterialInterface* baseMat;
+
+	// Dynamic Material Instance
+	UPROPERTY()
+		UMaterialInstanceDynamic* dmiMat;
+
+	// Overlap Begin Function
+	UFUNCTION()
+		void OnOverlapBegin(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+};
